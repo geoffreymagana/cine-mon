ed2520ac33c936536de421affa6630f9d84eb3d1
Geoffrey Gitau <geoffreymagana21@gmail.com>
2025-07-02 01:26:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/app/analytics/page.tsx
+++ src/app/app/analytics/page.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import * as React from 'react';
+import Link from 'next/link';
 import { 
     Area,
     AreaChart,
@@ -566,3 +567,5 @@ export default function AnalyticsPage() {
         </>
     )
 }
+
+    
